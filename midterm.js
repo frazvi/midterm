affc25d9ecb7012ffb77c3484abf6e2a3b434acb
frazvi <fatimarazvi@gmail.com>
2016-02-09 17:51:38 -0700

    commit

--- midterm.js
+++ midterm.js
@@ -3,5 +3,26 @@ angular.module("midtermApp", []);
 angular
 	.module("midtermApp")
 	.controller("midtermTroller", ['$scope', function($scope) {
+	
+		$scope.shouldShowLB = true
+
+		$scope.shouldShow = function () {
+			return $scope.shouldShowLB
+		}
+
+		$scope.goHome = function () {
+			$scope.shouldShowLB = false
+		}
+
+		$scope.randomPath = function () {
+			var randomIndex = Math.random($scope.paths.length - 1)
+			console.log(randomIndex);
+			return $scope.paths[randomIndex]
+		}
+		$scope.paths = [
+			"question",
+			"videoOne",
+			"videoTwo"
+		]
 
 	}])
